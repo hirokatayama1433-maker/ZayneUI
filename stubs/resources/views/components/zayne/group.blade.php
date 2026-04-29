40e93f4aa540ad7ca80a629f879f1e91756041c4
Zaynne <hirokatayama1433@gmail.com>
2026-04-29 21:05:07 +0800

    sdf

--- stubs/resources/views/components/zayne/group.blade.php
+++ stubs/resources/views/components/zayne/group.blade.php
@@ -3,11 +3,11 @@
     'label'   => '', 
 ])
 
-<div class="flex flex-col gap-2 px-2 py-1">
+<div class="flex flex-col gap-2">
     @if($label)
         <span
             title="{{ $label }}"
-            class="flex w-full min-w-0 truncate text-[11px] font-medium uppercase tracking-[0.16em] text-[var(--zayne-custom-sidebar-content)]/55 transition-all duration-200 group-data-[sidebar=collapsed]:max-w-[34px] group-data-[sidebar=collapsed]:text-[9px] group-data-[sidebar=collapsed]:tracking-[0.08em]"
+            class="flex w-full min-w-0 truncate text-[11px] font-medium uppercase tracking-[0.16em] text-[var(--zayne-custom-sidebar-content)]/55 transition-all duration-200 group-data-[sidebar=collapsed]:max-w-[34px] group-data-[sidebar=collapsed]:tracking-[0.08em]"
         >
             {{ $label }}
         </span>
